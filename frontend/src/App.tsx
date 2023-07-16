e8f16c328e01ae7caee993c47221f5d68c567550
JLake310 <0310kjy@gmail.com>
2023-07-17 03:03:18 +0900

    [Feat] Add frontend codes

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import StartPage from "@pages/StartPage";
+import SearchPage from "@pages/SearchPage";
+import ResultPage from "@pages/ResultPage";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StartPage />}></Route>
+          <Route path="/search" element={<SearchPage />}></Route>
+          <Route path="/result" element={<ResultPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/styles/keyframes.ts
@@ -0,0 +1,53 @@
+import { keyframes } from "styled-components";
+
+export const start2 = keyframes`
+0%{
+  transform: translate(0,20%);
+  opacity: 0;
+}
+100%{
+  transform: translate(0%,0);
+  opacity: 1;
+}
+`;
+
+export const startEnd = keyframes`
+0%{
+  transform: translate(0,80%);
+  opacity: 0;
+}
+20%{
+  transform: translate(0,0%);
+  opacity: 1;
+}
+80%{
+  transform: translate(0,0%);
+  opacity: 1;
+}
+100%{
+  transform: translate(0%,80%);
+  opacity: 0;
+}
+`;
+
+export const start = keyframes`
+0%{
+  transform: translate(0,80%);
+  opacity: 0;
+}
+100%{
+  transform: translate(0%,0);
+  opacity: 1;
+}
+`;
+
+export const end = keyframes`
+0%{
+  transform: translate(0,0);
+  opacity: 1;
+}
+100%{
+  transform: translate(0%,80%);
+  opacity: 0;
+}
+`;
