d3d3b26ba7d14d563c549fd38de9e03c1e0174bb
JLake310 <0310kjy@gmail.com>
2023-07-17 10:57:40 +0900

    [Feat] backend 코드 수정 및 requirements 수정

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ import pymysql
 import os
 from fastapi import FastAPI
 from pydantic import BaseModel
-import configparser
 import uvicorn
 from typing import List, Union, Optional, Dict, Any
 import re
@@ -74,16 +73,14 @@ def create_conn():
 
 @app.get("/api/product/prod_name/{prod_name}")
 async def search_product(prod_name: str):
-    """
-    제품명 검색 API
+    '''
+    제품명 검색 API (LIKE)
     :param prod_name:
     :return:
-    """
+    '''
     conn = create_conn()
     curs = conn.cursor()
-    curs.execute(
-        "SELECT * FROM products WHERE prod_name LIKE %s", ("%" + prod_name + "%",)
-    )
+    curs.execute("SELECT * FROM products WHERE prod_name LIKE %s", ('%' + prod_name + '%',))
     results = curs.fetchall()
     conn.close()
 
@@ -99,14 +96,14 @@ async def search_product(prod_name: str):
 
 @app.get("/api/reviews/prod_name/{prod_name}")
 def read_reviews(prod_name: str):
-    """
-    제품명으로 리뷰 검색 API
+    '''
+    정확히 일치하는 제품명 관련 리뷰 찾기 API (일치)
     :param prod_name:
     :return:
-    """
+    '''
     conn = create_conn()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM reviews WHERE prod_name = %s", (prod_name,))
+    cursor.execute("SELECT * FROM reviews_ver31 WHERE prod_name = %s", (prod_name,))
     result = cursor.fetchall()
 
     if len(result) == 0:
@@ -115,31 +112,61 @@ def read_reviews(prod_name: str):
 
     reviews = []
     for row in result:
-        reviews.append(
-            {
-                "prod_id": row[1],
-                "rating": row[3],
-                "title": row[4],
-                "context": row[5],
-                "answer": row[6],
-                "review_url": row[7],
-            }
-        )
+        reviews.append({
+            "prod_id": row[1],
+            "prod_name" : row[2],
+            "rating": row[3],
+            "title": row[4],
+            "context": row[5],
+            "answer": row[6],
+            "review_url": row[7]
+        })
 
     conn.close()
     return {"reviews": reviews}
 
 
-@app.get("/api/reviews/all")
-def read_reviews():
-    """
-    제품명으로 리뷰 검색 API
+@app.get("/api/reviews/search/prod_name/{prod_name}")
+def read_reviews(prod_name: str):
+    '''
+    제품명으로 리뷰 검색 API (LIKE)
     :param prod_name:
     :return:
-    """
+    '''
     conn = create_conn()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM reviews_ver3")
+    cursor.execute("SELECT * FROM reviews_ver31 WHERE prod_name LIKE %s", ('%' + prod_name + '%',))
+    result = cursor.fetchall()
+
+    if len(result) == 0:
+        conn.close()
+        return {"error": f"No product found for name: {prod_name}"}
+
+    reviews = []
+    for row in result:
+        reviews.append({
+            "prod_id": row[1],
+            "prod_name": row[2],
+            "rating": row[3],
+            "title": row[4],
+            "context": row[5],
+            "answer": row[6],
+            "review_url": row[7]
+        })
+
+    conn.close()
+    return {"reviews": reviews}
+
+@app.get("/api/reviews/all")
+def read_reviews():
+    '''
+    리뷰 전부 가져오기
+    :param prod_name:
+    :return:
+    '''
+    conn = create_conn()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM reviews_ver31")
     result = cursor.fetchall()
 
     if len(result) == 0:
@@ -148,62 +175,18 @@ def read_reviews():
 
     reviews = []
     for row in result:
-        reviews.append(
-            {
-                "prod_id": row[1],
-                "rating": row[3],
-                "title": row[4],
-                "context": row[5],
-                "answer": row[6],
-                "review_url": row[7],
-            }
-        )
+        reviews.append({
+            "prod_id": row[1],
+            "prod_name": row[2],
+            "rating": row[3],
+            "title": row[4],
+            "context": row[5],
+            "answer": row[6],
+            "review_url": row[7]
+        })
 
     conn.close()
     return {"reviews": reviews}
-
-
-@app.get("/api/reviews/search/prod_name/{prod_name}")
-def read_reviews(prod_name: str):
-    """
-    제품명으로 리뷰 검색 API (LIKE)
-    :param prod_name:
-    :return:
-    """
-    conn = create_conn()
-    cursor = conn.cursor()
-    cursor.execute(
-        "SELECT * FROM reviews_ver5 WHERE prod_name LIKE %s", ("%" + prod_name + "%",)
-    )
-    result = cursor.fetchall()
-
-    if len(result) == 0:
-        conn.close()
-        return {"error": f"No product found for name: {prod_name}"}
-
-    reviews = []
-    for row in result:
-        reviews.append(
-            {
-                "prod_id": row[1],
-                "prod_name": row[2],
-                "rating": row[3],
-                "title": row[4],
-                "context": row[5],
-                "answer": row[6],
-                "review_url": row[7],
-            }
-        )
-
-    conn.close()
-
-    # return {"reviews": dict_to_list(reviews)}
-    return {"reviews": reviews}
-
-
-@app.get("/api/")
-def read_root():
-    return {"Hello": "World"}
 
 
 if __name__ == "__main__":
