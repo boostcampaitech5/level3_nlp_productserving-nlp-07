803581826cc6fac8e30c7a6b5273883b3e252cff
JLake310 <0310kjy@gmail.com>
2023-07-19 02:30:32 +0900

    [Fix] 리뷰 페이지 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import StartPage from "@pages/StartPage";
 import SearchPage from "@pages/SearchPage";
 import ResultPage from "@pages/ResultPage";
-import ReviewPage from "@pages/ReviewPage";
 import { RecoilRoot } from "recoil";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
             <Route path="/" element={<StartPage />}></Route>
             <Route path="/search" element={<SearchPage />}></Route>
             <Route path="/result" element={<ResultPage />}></Route>
-            <Route path="/review" element={<ReviewPage />}></Route>
           </Routes>
         </BrowserRouter>
       </div>
