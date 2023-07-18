3e1d5d3e56874409354490ed7c4149890968a40d
JLake310 <0310kjy@gmail.com>
2023-07-19 02:31:18 +0900

    [Feat] 더미데이터로 결과 페이지 추가

--- frontend/src/components/pages/ResultPage.tsx
+++ frontend/src/components/pages/ResultPage.tsx
@@ -4,86 +4,201 @@ import styled, { css } from "styled-components";
 import check from "@assets/images/check.png";
 import * as KF from "@styles/keyframes";
 import axios from "axios";
-import { DBDataType } from "@apis/DB_api";
 import Spinner from "@blocks/Spinner";
+import { useRecoilState } from "recoil";
+import { userState } from "atoms/userState";
+import EmptyHeart from "@assets/images/empty_heart.png";
+import FillHeart from "@assets/images/fill_heart.png";
 
-interface ProgressType {
-  progress: number;
-}
+const isMobile = function () {
+  const match = window.matchMedia("(pointer:coarse)");
+  return match && match.matches;
+};
+
+type DPRReviewType = {
+  review: [
+    { prod_id: string; context: string; prod_name: string },
+    { prod_id: string; context: string; prod_name: string },
+    { prod_id: string; context: string; prod_name: string }
+  ];
+};
 
 interface LoadingType {
   isdone: number;
 }
 
 const ResultPage = () => {
-  const [curProgress, setProgress] = useState(0);
-  const [isLoaded, setLoaded] = useState(false);
-  const [showResult, setResult] = useState(false);
-  const [db_data, setData] = useState<DBDataType | null>(null);
   const [db_loaded, setDBLoaded] = useState(false);
   const [retrieve_loaded, setRetrieveLoaded] = useState(false);
-  // const [retrieve_result, setRetrieveResult] = useState("");
   const [summary_loaded, setSummaryLoaded] = useState(false);
-  const [summary_result, setSummaryResult] = useState("");
+  const [prodAndQuery] = useRecoilState(userState);
+  const [prodIDList, setIDList] = useState<number[]>([0, 0, 0]);
+  const [summaryCount, setSummaryCount] = useState(1);
 
-  var product = localStorage.getItem("itemName");
-  var query = localStorage.getItem("itemOption");
+  // const [isLoaded, setLoaded] = useState(false);
+  // const [showResult, setResult] = useState(false);
+  // const [summaryList, setSummaryList] = useState<string[]>(["", "", ""]);
+  // const [imgURLs, setURLs] = useState<string[]>(["", "", ""]);
+  // const [imgLinks, setLinks] = useState<string[]>(["", "", ""]);
+  // const [prodNames, setProdNames] = useState<string[]>(["", "", ""]);
+
+  // 개발용 더미 데이터
+  const [isLoaded, setLoaded] = useState(true);
+  const [showResult, setResult] = useState(true);
+  const [prodNames, setProdNames] = useState<string[]>([
+    "떡볶이 추억의 국민학교 떡볶이 오리지널 (냉동), 600g, 2개",
+    "떡볶이 풀무원 쌀 순쌀 고추장 떡볶이, 480g, 2개",
+    "떡볶이 오뚜기 맛있는 국물 떡볶이, 424g, 2개",
+  ]);
+  const [summaryList, setSummaryList] = useState<string[]>([
+    "<맛> 맛있어요 <양> 30개씩 주문하면 너무 많고 10개씩 주문하면 한 달 먹기 딱 좋아요 <조리> 날 계란을 넣어 주시고 수프를 넣어 주세요 <맛> 면도 부드럽고 국물도 더욱 구수해집니다",
+    "<맛> 맛있어요 <양> 30개씩 주문하면 너무 많고 10개씩 주문하면 한 달 먹기 딱 좋아요 <조리> 날 계란을 넣어 주시고 수프를 넣어 주세요 <맛> 면도 부드럽고 국물도 더욱 구수해집니다",
+    "<맛> 맛있어요 <양> 30개씩 주문하면 너무 많고 10개씩 주문하면 한 달 먹기 딱 좋아요 <조리> 날 계란을 넣어 주시고 수프를 넣어 주세요 <맛> 면도 부드럽고 국물도 더욱 구수해집니다",
+  ]);
+  const [imgURLs, setURLs] = useState<string[]>([
+    "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/544327073118021-b03a2183-a488-4489-b339-8003413aba29.jpg",
+    "https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/c1dbdf3d-01c9-423e-9b60-62619350367d4627553494027830654.png",
+    "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/ef21b827-dabe-4592-bdfd-8942cc292de52715430314288738577.png",
+  ]);
+  const [imgLinks, setLinks] = useState<string[]>([
+    "https://www.coupang.com/vp/products/115734217?itemId=351022598&vendorItemId=3855510397&pickType=COU_PICK&q=%EB%96%A1%EB%B3%B6%EC%9D%B4&itemsCount=36&searchId=b321d5a153ba44fea576741a065a5a01&rank=1",
+    "https://www.coupang.com/vp/products/111269049?itemId=19272258486&vendorItemId=86387426969&q=%EB%96%A1%EB%B3%B6%EC%9D%B4&itemsCount=36&searchId=b321d5a153ba44fea576741a065a5a01&rank=9",
+    "https://www.coupang.com/vp/products/111244392?itemId=19282503148&vendorItemId=86397459992&q=%EB%96%A1%EB%B3%B6%EC%9D%B4&itemsCount=36&searchId=b321d5a153ba44fea576741a065a5a01&rank=12",
+  ]);
+
+  const [FirstHeart, setFirstHeart] = useState(false);
+  const [SecondHeart, setSecondHeart] = useState(false);
+  const [ThirdHeart, setThirdHeart] = useState(false);
+  const [isProdSelected, setSelected] = useState(false);
+  const heartPushed = [FirstHeart, SecondHeart, ThirdHeart];
+  const heartPushHandler = [setFirstHeart, setSecondHeart, setThirdHeart];
+
+  const [isDescModalOn, setDescModal] = useState(true);
 
   useEffect(() => {
     const FetchData = async () => {
       await axios({
         method: "get",
-        url: "/api/reviews/search/prod_name/" + product,
-      }).then((response) => {
-        setData(response.data);
-        setDBLoaded(true);
-        axios({
-          method: "post",
-          url: process.env.REACT_APP_DPR_ENDPOINT + "/dpr/split_db",
-          headers: {
-            Accept: "*/*",
-            "Content-Type": "application/json",
-          },
-          data: {
-            query: query,
-            reviews: response.data.reviews,
-          },
-        }).then((dpr_response) => {
-          setRetrieveLoaded(true);
-          axios({
-            method: "post",
-            url: process.env.REACT_APP_SUMMARY_ENDPOINT + "/summary",
-            headers: {
-              Accept: "*/*",
-              "Content-Type": "application/json",
-            },
-            data: [dpr_response.data.review],
-          }).then((summary_response) => {
-            setSummaryResult(summary_response.data);
-            setSummaryLoaded(true);
-            setTimeout(() => setLoaded(true), 2000);
-            setTimeout(() => setResult(true), 6200);
-          });
+        url:
+          "http://localhost:8080/api/reviews/search/prod_name/" +
+          prodAndQuery["production"] +
+          " ",
+      })
+        .then((response) => {
+          if (response.data.source === "crawl") {
+            setDBLoaded(true);
+            axios({
+              method: "post",
+              url: process.env.REACT_APP_DPR_ENDPOINT + "/dpr/split_db",
+              headers: {
+                Accept: "*/*",
+                "Content-Type": "application/json",
+              },
+              data: {
+                query: prodAndQuery["query"],
+                reviews: response.data.reviews,
+              },
+            })
+              .then((dprResponse) => {
+                setRetrieveLoaded(true);
+                const dprList: DPRReviewType = dprResponse.data;
+                setIDList([
+                  parseInt(dprList.review[0].prod_id),
+                  parseInt(dprList.review[1].prod_id),
+                  parseInt(dprList.review[2].prod_id),
+                ]);
+
+                setProdNames([
+                  dprList.review[0].prod_name,
+                  dprList.review[1].prod_name,
+                  dprList.review[2].prod_name,
+                ]);
+                const textList = [
+                  dprList.review[0].context,
+                  dprList.review[1].context,
+                  dprList.review[2].context,
+                ];
+                // eslint-disable-next-line array-callback-return
+                textList.map(async (review: string, idx: number) => {
+                  await axios({
+                    method: "post",
+                    url: process.env.REACT_APP_SUMMARY_ENDPOINT + "/summary",
+                    headers: {
+                      Accept: "*/*",
+                      "Content-Type": "application/json",
+                    },
+                    data: [review],
+                  })
+                    .then((summary_response) => {
+                      const curSummaryList = summaryList;
+                      curSummaryList[idx] = summary_response.data.review;
+                      setSummaryList(summaryList);
+                      setSummaryCount(summaryCount + 1);
+                      // setSummaryResult(summaryList);
+                      // setSummaryLoaded(true);
+                      // setTimeout(() => setLoaded(true), 2000);
+                      // setTimeout(() => setResult(true), 6200);
+                    })
+                    .catch((error) => {
+                      console.log("요약 에러");
+                    });
+                });
+
+                // 요약 끝
+                setSummaryLoaded(true);
+                setTimeout(() => setLoaded(true), 2000);
+                setTimeout(() => setResult(true), 6200);
+
+                // axios({
+                //   method: "post",
+                //   url: process.env.REACT_APP_SUMMARY_ENDPOINT + "/summary",
+                //   headers: {
+                //     Accept: "*/*",
+                //     "Content-Type": "application/json",
+                //   },
+                //   data: [textList],
+                // })
+                //   .then((summary_response) => {
+                //     const summaryList = summary_response.data.review;
+                //     setSummaryResult(summaryList);
+                //     setSummaryLoaded(true);
+                //     setTimeout(() => setLoaded(true), 2000);
+                //     setTimeout(() => setResult(true), 6200);
+                //   })
+                //   .catch((error) => {
+                //     console.log("요약 에러");
+                //   });
+              })
+              .catch((error) => {
+                console.log("DPR 에러");
+              });
+          } else {
+            // db에서 가져옴
+          }
+        })
+        .catch((error) => {
+          console.log("DB 에러");
         });
-      });
     };
-    FetchData();
+    // FetchData();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // useEffect(()=>{
-  //   // setTimeout(() => {
-  //   //   for (let i = 0; i <= 100; i += 1) {
-  //   //     setTimeout(() => setProgress(curProgress + i), 50 * i);
-  //   //   }
-  //   // }, 1400);
-  // })
+  const HeartHandler = (idx: number) => {
+    if (!isProdSelected && !isDescModalOn) {
+      heartPushHandler[idx](true);
+      setSelected(true);
+      const data = {
+        query: prodAndQuery["query"],
+        recommendations: prodIDList,
+        best: prodIDList[idx],
+        review: "",
+      };
 
-  if (curProgress === 100) {
-    setTimeout(() => setLoaded(true), 2000);
-    setTimeout(() => setResult(true), 6200);
-  }
+      // 피드백 API 호출
+    }
+  };
 
   if (!showResult && !isLoaded) {
     return (
@@ -96,17 +211,12 @@ const ResultPage = () => {
                 ? "리뷰 데이터를 수집하고 있어요"
                 : !retrieve_loaded
                 ? "리뷰 데이터를 분석하고 있어요"
-                : "리뷰 데이터를 요약하고 있어요"}
+                : "리뷰 데이터를 요약하고 있어요\n(" +
+                  summaryCount.toString() +
+                  "/3)"}
             </LodingText>
             <Spinner />
           </LodingDiv>
-          {/* <LodingCenterText progress={curProgress}>
-            상품을 찾고 있어요.
-          </LodingCenterText>
-          <ProgressBarDiv progress={curProgress}>
-            <ProgressBar progress={curProgress} />
-            <ProgressText>{curProgress}%</ProgressText>
-          </ProgressBarDiv> */}
         </CenterWrapper>
       </>
     );
@@ -124,31 +234,60 @@ const ResultPage = () => {
   }
 
   return (
-    <>
+    <MainWrapper>
       <HeaderLogoBlock />
-      <ItemDiv></ItemDiv>
-      <TextDiv>
-        <Description>
-          원하시는 조건에 맞는 상품을 찾아왔어요.
-          <br />
-          상품에 대한 리뷰 요약도 보여드릴게요.
-          <br />
-          <br />
-          {/* 원하시는 조건에 맞는 상품을 찾아왔어요.
-        <br />위 상품은 이렇고, 저렇습니다. (상품 설명 요약) */}
-          {summary_result}
-          {/* &lt;맛&gt; 맛있어요 &lt;양&gt; 30개씩 주문하면 너무 많고 10개씩
-          주문하면 한 달 먹기 딱 좋아요 &lt;조리&gt; 날 계란을 넣어 주시고
-          수프를 넣어 주세요 &lt;맛&gt; 면도 부드럽고 국물도 더욱 구수해집니다
-          &lt;추천&gt; 추운 겨울 백제 쌀국수로 따뜻하고 시원하게 즐겨보세요
-          &lt;맛&gt; 달달하고 계속 당기는 그 맛 &lt;양&gt; 성인 두 분이 드시기에
-          딱 좋은 양 &lt;조리&gt; 물 또는 보리차 넣어서 끓이라고 써있는데
-          떡본연의 맛 때문에 그런것인가 &lt;선호&gt; 아이가 어려서 많이
-          매워한다면 체다치즈 한장 넣으면 매운게 훨씬 덜 함 &lt;향&gt; 추억의
-          국물떡볶이 */}
-        </Description>
-      </TextDiv>
-    </>
+      <SummaryWrapper>
+        {[0, 1, 2].map((idx) => (
+          <SummaryDiv key={idx}>
+            <ItemDiv>
+              <ProductImg
+                src={imgURLs[idx]}
+                alt=""
+                onClick={() => window.open(imgLinks[idx])}
+              />
+              <ProductTitleText>{prodNames[idx]}</ProductTitleText>
+              <FeedBackDiv>
+                <HeartImg
+                  src={heartPushed[idx] ? FillHeart : EmptyHeart}
+                  alt=""
+                  onClick={() => HeartHandler(idx)}
+                />
+              </FeedBackDiv>
+            </ItemDiv>
+            <TextDiv>
+              <Description>{summaryList[idx]}</Description>
+            </TextDiv>
+          </SummaryDiv>
+        ))}
+      </SummaryWrapper>
+      {isDescModalOn && (
+        <DescModal>
+          <HeightBox />
+          <DescText>
+            추천 받은 상품들 중 <br />
+            가장 마음에 드는 상품을 골라주세요! <br />
+            <DescSmallText>
+              (상품 카드를 클릭하면 상품 페이지로 이동합니다)
+            </DescSmallText>
+            <br />
+            <br />
+            <DescLightText>
+              한 번만 고를 수 있으니 <br />
+              신중히 골라주세요 ☺️
+            </DescLightText>
+            <br />
+          </DescText>
+          <DescButton onClick={() => setDescModal(false)}>
+            확인했어요
+          </DescButton>
+        </DescModal>
+      )}
+      {isProdSelected && (
+        <ReviewModal>
+          <DescText>리뷰에 참여해주셔서 감사해요! 😙</DescText>
+        </ReviewModal>
+      )}
+    </MainWrapper>
   );
 };
 
@@ -165,26 +304,6 @@ const CenterWrapper = styled.div`
   justify-content: space-between;
 `;
 
-const LodingCenterText = styled.span<ProgressType>`
-  color: #4a4a4a;
-  text-align: center;
-  font-size: 36rem;
-  font-family: Pretendard;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-
-  ${css`
-    animation: ${KF.start} 0.8s 0.2s 1 both;
-  `}
-
-  ${(props) =>
-    props.progress === 100 &&
-    css`
-      animation: ${KF.end} 0.8s 1.2s 1 both;
-    `}
-`;
-
 const CenterText = styled.span`
   color: #4a4a4a;
   text-align: center;
@@ -197,48 +316,6 @@ const CenterText = styled.span`
   ${css`
     animation: ${KF.startEnd} 4s 0s 1 both;
   `}
-`;
-
-const ProgressBarDiv = styled.div<ProgressType>`
-  width: 700rem;
-  height: 74rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-
-  ${css`
-    animation: ${KF.start} 0.8s 0.6s 1 both;
-  `}
-
-  ${(props) =>
-    props.progress === 100 &&
-    css`
-      animation: ${KF.end} 0.8s 1s 1 both;
-    `}
-`;
-
-const ProgressBar = styled.div<ProgressType>`
-  width: 700rem;
-  height: 30rem;
-  background: linear-gradient(
-    90deg,
-    #4b81bf ${(props) => props.progress}%,
-    #ffffff ${(props) => props.progress}%
-  );
-
-  border-radius: 20rem;
-  box-shadow: 0rem 0rem 17rem 0rem rgba(0, 0, 0, 0.25);
-`;
-
-const ProgressText = styled.span`
-  color: #4a4a4a;
-  text-align: center;
-  font-size: 25rem;
-  font-family: Pretendard;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
 `;
 
 const CheckImg = styled.img`
@@ -261,9 +338,15 @@ const ItemDiv = styled.div`
   background: #fff;
   box-shadow: 0rem 0rem 17rem 0rem rgba(0, 0, 0, 0.25);
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+
   ${css`
     animation: ${KF.start2} 0.8s 0.2s 1 both;
   `}
+  ${isMobile() && "width: 300rem; height: 400rem; margin-top: 30rem;"}
 `;
 
 const Description = styled.span`
@@ -276,6 +359,9 @@ const Description = styled.span`
   font-weight: 500;
   line-height: 150%;
   width: 200rem;
+  word-break: keep-all;
+
+  ${isMobile() && "font-size: 20rem;"}
   ${css`
     animation: ${KF.start} 0.8s 0.4s 1 both;
   `};
@@ -309,6 +395,167 @@ const LodingDiv = styled.div<LoadingType>`
 `;
 
 const TextDiv = styled.div`
-  width: 90vw;
+  width: 417rem;
   margin: 0 auto;
+  margin-bottom: 30rem;
+  ${isMobile() && "width: 300rem;"}
+`;
+
+const SummaryWrapper = styled.div`
+  display: flex;
+  width: 80vw;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: 1400px) {
+    flex-direction: column;
+  }
+`;
+
+const SummaryDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 50rem;
+`;
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+`;
+
+const ProductImg = styled.img`
+  width: 350rem;
+  height: 350rem;
+  margin-top: 30rem;
+  border-radius: 20rem;
+  ${isMobile() && "width: 270rem; height: 270rem; margin-top: 20rem;"}
+`;
+
+const ProductTitleText = styled.span`
+  font-size: 26rem;
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  width: 90%;
+  margin-top: 30rem;
+  word-break: keep-all;
+  ${isMobile() && "font-size: 16rem; margin-top: 20rem;"}
+`;
+
+const FeedBackDiv = styled.div`
+  height: 10rem;
+  display: flex;
+  justify-content: flex-end;
+  width: 90%;
+  margin-top: 20rem;
+  ${isMobile() && "margin-top: 5rem;"}
+`;
+
+const HeartImg = styled.img`
+  width: 30rem;
+  height: 30rem;
+`;
+
+const DescModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: 400rem;
+  height: 400rem;
+  background: white;
+
+  // border: 5rem solid #f4f4f4;
+  border-radius: 20rem;
+  box-shadow: 0rem 0rem 17rem 0rem rgba(0, 0, 0, 0.25);
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+
+  ${css`
+    animation: ${KF.opacity_change} 0.8s 0s 1 both;
+  `};
+  ${isMobile() && "width: 300rem; height:300rem;"}
+`;
+
+const DescText = styled.span`
+  font-size: 24rem;
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  width: 90%;
+  word-break: keep-all;
+  ${css`
+    animation: ${KF.opacity_change} 0.8s 0s 1 both;
+  `};
+  ${isMobile() && "font-size: 18rem;"}
+`;
+
+const DescSmallText = styled.span`
+  font-weight: 500;
+  font-size: 18rem;
+  ${isMobile() && "font-size: 14rem;"}
+`;
+
+const DescLightText = styled.span`
+  font-weight: 500;
+`;
+
+const DescButton = styled.div`
+  width: 200rem;
+  height: 50rem;
+  border-radius: 15rem;
+  border: 5rem solid var(--main-blue, #4b81bf);
+  background: var(--main-blue, #4b81bf);
+  color: #fff;
+  text-align: center;
+  font-size: 22rem;
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 30rem;
+
+  ${isMobile() &&
+  "font-size: 18rem; width: 150rem; height: 45rem; margin-bottom: 20rem;"}
+`;
+
+const HeightBox = styled.div`
+  height: 50rem;
+  ${isMobile() && "height: 55rem;"}
+`;
+
+const ReviewModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: 400rem;
+  height: 400rem;
+  background: white;
+
+  border-radius: 20rem;
+  box-shadow: 0rem 0rem 17rem 0rem rgba(0, 0, 0, 0.25);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  ${css`
+    animation: ${KF.opacity_change} 0.8s 0s 1 both;
+    animation: ${KF.opacity_end} 0.8s 2s 1 both;
+  `};
+  ${isMobile() && "width: 300rem; height:300rem;"}
 `;
