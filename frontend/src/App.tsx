cf1dd0e549aa6f1505671f087bdc0331120df6c6
JLake310 <0310kjy@gmail.com>
2023-07-18 14:24:14 +0900

    [Feat] Recoil 세팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,23 @@ import "./App.css";
 import StartPage from "@pages/StartPage";
 import SearchPage from "@pages/SearchPage";
 import ResultPage from "@pages/ResultPage";
+import ReviewPage from "@pages/ReviewPage";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<StartPage />}></Route>
-          <Route path="/search" element={<SearchPage />}></Route>
-          <Route path="/result" element={<ResultPage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<StartPage />}></Route>
+            <Route path="/search" element={<SearchPage />}></Route>
+            <Route path="/result" element={<ResultPage />}></Route>
+            <Route path="/review" element={<ReviewPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </RecoilRoot>
   );
 }
 
