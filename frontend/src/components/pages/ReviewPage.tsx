8db2adbe75dc31e7050e387c9f730ad0ad014c42
JLake310 <0310kjy@gmail.com>
2023-07-18 14:24:36 +0900

    [Feat] 리뷰 페이지 템플릿 추가

---
+++ frontend/src/components/pages/ReviewPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const ReviewPage = () => {
+  return (
+    <div>
+      <span>리뷰 페이지</span>
+    </div>
+  );
+};
+
+export default ReviewPage;
