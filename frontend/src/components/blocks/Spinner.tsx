213c6767e28f25fd7484469eebff41812f7c8f80
JLake310 <0310kjy@gmail.com>
2023-07-21 16:52:54 +0900

    [Feat] 로딩페이지 텍스트 슬라이더 구현

--- frontend/src/components/blocks/Spinner.tsx
+++ frontend/src/components/blocks/Spinner.tsx
@@ -3,7 +3,7 @@ import "./Spinner.css";
 
 const Spinner = () => {
   return (
-    <div className="sk-chase">
+    <div className="sk-chase" style={{ marginTop: "20rem" }}>
       <div className="sk-chase-dot"></div>
       <div className="sk-chase-dot"></div>
       <div className="sk-chase-dot"></div>
